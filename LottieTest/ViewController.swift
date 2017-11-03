2461333f815e2ff18ec32da4058b2065fc6d03ff
Barlow Tucker <barlowtucker@gmail.com>
2017-11-03 15:19:27 -0600

    Crashes lottie

--- LottieTest/ViewController.swift
+++ LottieTest/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var lottieView: UIView!
+    
+    var animation:LOTAnimationView!
+    var iterations = 3000
+    var isRunning = false
+    var goBack = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.webView.loadHTMLString("<ul><li><strong>Easy setup:</strong>  Just add a PhotoBrowserView to your ViewController and implement the datasource and delegate</li><li><strong>Full Screen Browser:</strong> Tap on an image in the PhotoView to launch the full screen browser.</li><li><strong>Swipe Gestures:</strong> Close the full screen browser by tapping the close button, tapping the image, or swiping up.</li><li><strong>Swift:</strong> This project was writen completely in Swift.</li></ul>", baseURL: nil)
+        self.startAnimation()
+        guard goBack else { self.push(); return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            self.navigationController?.popViewController(animated: false)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func startAnimation() {
+        let animation = LOTAnimationView(name: "load")
+        self.animation = animation
+        self.lottieView.subviews.forEach{ $0.removeFromSuperview() }
+        self.lottieView.addSubview(self.animation)
+        
+        self.animation.loopAnimation = true
+        self.animation.play()
+    }
+    
+    @IBAction func goTapped(_ sender: Any) {
+        self.isRunning = true
+        self.iterations = 3000
+        self.push()
+    }
+    
+    func push() {
+        guard self.isRunning && self.iterations > 0 else { return }
+        self.iterations -= 1
+        self.navigationItem.title = "\(self.iterations)"
+        
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "lottieView") as? ViewController else { return }
+        viewController.goBack = true
+        viewController.iterations = self.iterations
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            self.navigationController?.pushViewController(viewController, animated: false)
+        }
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+plugin 'cocoapods-wholemodule'
+
+# ignore all warnings from all pods
+inhibit_all_warnings!
+use_frameworks!
+
+target 'LottieTest' do
+    
+    pod 'lottie-ios', '= 2.1.3'
+    
+end
